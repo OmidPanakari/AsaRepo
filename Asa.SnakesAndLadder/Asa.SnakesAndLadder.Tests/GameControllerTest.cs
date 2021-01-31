ef3c4482c73748c5309c97e401104616d72459f1
Omid <omid.panakari.s@gmail.com>
2021-01-31 08:30:21 +0330

    Added Controller tests

---
+++ Asa.SnakesAndLadder/Asa.SnakesAndLadder.Tests/GameControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Asa.SnakesAndLadder.Core;
+
+namespace Asa.SnakesAndLadder.Tests
+{
+	public class GameControllerTest
+	{
+		[SetUp]
+		public void SetUp()
+		{
+
+		}
+
+		[Test]
+		public void ErrorIfMoreThan4Players()
+		{
+			var game = new GameController(5, 5, 3, 4);
+			Assert.Throws<ArgumentOutOfRangeException>(() => game.CheckPlayerCount(5));
+		}
+
+		[Test]
+		public void ErrorIfLessThan2Players()
+		{
+			var game = new GameController(5, 5, 3, 4);
+			Assert.Throws<ArgumentOutOfRangeException>(() => game.CheckPlayerCount(1));
+		}
+
+		[Test]
+		public void ErrorIfWrongColor()
+		{
+			var game = new GameController(5, 5, 3, 4);
+			Assert.Throws<ArgumentException>(() => game.AddPlayer("Omid", "Gray"));
+		}
+	}
+}
